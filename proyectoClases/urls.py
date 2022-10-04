c4f603c36495d070edffdecde656e59c026b98a7
Ezequiel Walter <eze_w@hotmail.com>
2022-10-04 19:07:03 -0300

    Clase Django - Portfolio (Parte 2)

--- proyectoClases/urls.py
+++ proyectoClases/urls.py
@@ -20,7 +20,11 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hola/', views.hola),
-    path('fecha /', views.fecha),
+    path('fecha/', views.fecha),
     path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
     path('mi-template/', views.mi_template),
+    path('mi-template/<str:nombre>', views.tu_template),
+    path('prueba-template/', views.prueba_template),
+    path('ver-familiares/', views.ver_familiares),
+    path('crear-familiar/<str:nombre>/<str:apellido>/', views.crear_familiar),
 ]
